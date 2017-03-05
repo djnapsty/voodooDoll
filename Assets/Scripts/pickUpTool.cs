ea1fef522cf6f285a670ca73841044f0763f156d
djnapsty <dj_napsty@Justins-MacBook-Pro-2.local>
2017-03-04 22:11:44 -0800

    v1

---
+++ Assets/Scripts/pickUpTool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class pickUpTool : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnMouseDrag() 
+	{
+		float yAxisY =10.0f ;
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, yAxisY);
+		Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+		transform.position = objectPosition;
+	}
+}
