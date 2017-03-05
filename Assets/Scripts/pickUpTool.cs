9d515690032965a559110ca926df4c57d906c4e2
djnapsty <dj_napsty@Justins-MacBook-Pro-2.local>
2017-03-04 23:40:16 -0800

    v1.1

--- Assets/Scripts/pickUpTool.cs
+++ Assets/Scripts/pickUpTool.cs
@@ -8,7 +8,7 @@ public class pickUpTool : MonoBehaviour {
 	// Update is called once per frame
 	void OnMouseDrag() 
 	{
-		float yAxisY =10.0f ;
+		float yAxisY =47.0f ;
 		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, yAxisY);
 		Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
