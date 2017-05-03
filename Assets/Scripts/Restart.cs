3c7564f159afa719030083b74ba9983222895628
djnapsty <dj_napsty@Justins-MacBook-Pro-2.local>
2017-05-03 16:39:09 -0700

    reset button

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Restart : MonoBehaviour
+{
+
+	[SerializeField] private Button MyButton = null;
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) //click to destroy debug button
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+		}
+	}
+
+}
+
