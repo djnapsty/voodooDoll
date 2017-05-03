07bc98c15ff85092bd1fb981fe3c7febdfb3bb5c
djnapsty <dj_napsty@Justins-MacBook-Pro-2.local>
2017-05-03 15:49:21 -0700

    meeting stuff

---
+++ Assets/Scripts/buttondestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class buttondestroy : MonoBehaviour {
+
+	[SerializeField] private Button MyButton = null;
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) //click to destroy debug button
+		{
+			Destroy (MyButton.gameObject);
+		}
+	}
+
+}
