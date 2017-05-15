b24763c55c9ba5386c2a140a2cf781b1b3d6ea68
djnapsty <dj_napsty@Justins-MacBook-Pro-2.local>
2017-05-15 13:13:15 -0700

    v1

---
+++ Assets/Scripts/legfire.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class legfire : MonoBehaviour 
+{
+
+	public GameObject fire;
+	public GameObject leg;
+
+	void OnMouseOver(Collision y)
+	{
+		//if (y.gameObject == leg) {
+			
+			GameObject fireCopy = (GameObject)Instantiate(fire, transform.position, transform.rotation);
+		//}
+	}
+
+
+
+}
